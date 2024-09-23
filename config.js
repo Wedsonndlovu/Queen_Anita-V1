00563a0464f663105250db3417dbba99ee63dbb7
Juniorndlovu <wedsonndlovu5@gmail.com>
2024-09-24 01:30:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '263784263228'
+global.nomerowner = ["263784263228"]
 
 // Apikey 
 
